ba8a8e4103d6a8bb77907cbbef8d99dba07df5c0
Michael Matta <mmmatta11@gmail.com>
2017-10-31 22:10:55 -0400

    added game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed; // since it's public it shows up in editor!!
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start() // called on first frame
+	{
+		count = 0;
+		SetCountText();
+		winText.text = "";
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() //physics updates - use command + ' with highlighted thing for possible functions (Input, RigidBody)
+	{ 
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("Pick Up"))
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You win";
+		}
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () 
+	{ // not using forces - just need this baby
+		transform.Rotate(new Vector3(15,30,45) * Time.deltaTime); //framerate independent
+	}
+}
